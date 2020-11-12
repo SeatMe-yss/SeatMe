2a22d2c751ce7179490b24af8c83e0b477f3cdf4
Shani Shalel <shanishalel89@gmail.com>
2020-11-12 20:27:42 +0200

    work from shani 2

--- app/src/main/java/com/example/myapplication/login.java
+++ app/src/main/java/com/example/myapplication/login.java
@@ -3,21 +3,36 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class login extends AppCompatActivity implements View.OnClickListener {
+import java.text.DateFormatSymbols;
+
+public class login extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     Button sign_in;
+    Spinner spinner_type;
+    String[] types;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         sign_in=(Button)findViewById(R.id.login2);
         sign_in.setOnClickListener(this);
+        spinner_type=findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array.spinner_type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_type.setAdapter(adapter);
+        spinner_type.setOnItemSelectedListener(this);
 
     }
+
+
 
     @Override
     public void onClick(View v) {
@@ -25,6 +40,18 @@ public class login extends AppCompatActivity implements View.OnClickListener {
             Intent intent= new Intent(this,activities.class );
             startActivity(intent);
         }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text=parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
