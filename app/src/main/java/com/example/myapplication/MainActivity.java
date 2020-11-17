b11f4b4769daff78780b4b9d8558c3ae91132d2a
shanihayik <shanihayik@gmail.com>
2020-11-17 20:55:22 +0200

    start activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,34 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button login;
-    Button register;
-    TextView welcome;
+
+    //Button logo;
+    ImageButton logo;
+//    Button register;
+//    TextView welcome;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_register);
-        login=(Button)findViewById(R.id.sign_in);
-        register=(Button)findViewById(R.id.register1);
-        login.setOnClickListener(this);
-        register.setOnClickListener(this);
+        setContentView(R.layout.start);
+        logo=findViewById(R.id.imageButton2);
+        //register=(Button)findViewById(R.id.register1);
+        logo.setOnClickListener(this);
+        //register.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        if(v==login){
-            Intent intent= new Intent(this,login.class );
-            startActivity(intent);
-        }
-        else if(v==register){
-            Intent intent= new Intent(this,register.class) ;
-            startActivity(intent);
-        }
+        Intent intent= new Intent(this,login_register.class );
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/login_register.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class login_register extends AppCompatActivity implements View.OnClickListener{
+    Button login;
+    Button register;
+    TextView welcome;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_register);
+        login=(Button)findViewById(R.id.sign_in);
+        register=(Button)findViewById(R.id.register1);
+        login.setOnClickListener(this);
+        register.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==login){
+            Intent intent= new Intent(this,login.class );
+            startActivity(intent);
+        }
+        else if(v==register){
+            Intent intent= new Intent(this,register.class) ;
+            startActivity(intent);
+        }
+    }
+}
